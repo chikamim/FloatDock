5f809dd437dc9af7ad59b60150f3046e44f44550
popor <908891024@qq.com>
2020-09-17 10:45:15 +0800

    实现点击+号功能, command + 点击APPicon功能

--- FloatDock/FloatDockVC/Presenter/FloatDockVCPresenter.m
+++ FloatDock/FloatDockVC/Presenter/FloatDockVCPresenter.m
@@ -45,38 +45,45 @@
 
 #pragma mark - VC_DataSource
 - (NSInteger)collectionView:(NSCollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
-    //return self.view.appInfoEntity;
-    return self.view.appInfoEntity.appPathArray.count;
+    
+    return MAX(self.view.appInfoEntity.appPathArray.count, 1);
 }
 
 - (NSCollectionViewItem *)collectionView:(NSCollectionView *)collectionView itemForRepresentedObjectAtIndexPath:(NSIndexPath *)indexPath {
     AppIconItem *item = [collectionView makeItemWithIdentifier:AppIconItemKey forIndexPath:indexPath];
     //item.textLabel.stringValue = [NSString stringWithFormat:@"第%zi个", indexPath.item];
-    
-    NSString * str = self.view.appInfoEntity.appPathArray[indexPath.item];
-    // NSLog(@"str: %@", str);
-    if (str.length == 0) {
+    NSString * str;
+    if (self.view.appInfoEntity.appPathArray.count == 0) {
+        str = @"";
         [item.appBT setImage:[NSImage imageNamed:@"icon48"]];
-        
+        item.runningApp = nil;
+        item.activeIV.hidden = YES;
     } else {
-        // http://hk.uwenku.com/question/p-vrwwdiql-bnz.html
-        NSWorkspace *workspace = [NSWorkspace sharedWorkspace];
-        NSImage *finderIcon;
-        //= [workspace iconForFile:[workspace absolutePathForAppBundleWithIdentifier:@"com.apple.Finder"]];
-        finderIcon = [workspace iconForFile:[str substringFromIndex:7]];
-        //[finderIcon setSize:NSMakeSize(AppWidth, AppWidth)];
-        [finderIcon setSize:NSMakeSize(self.view.awt.appIconWidthNum.floatValue, self.view.awt.appIconWidthNum.floatValue)];
+        str = self.view.appInfoEntity.appPathArray[indexPath.item];
+        // NSLog(@"str: %@", str);
+        if (str.length == 0) {
+            [item.appBT setImage:[NSImage imageNamed:@"icon48"]];
+            
+        } else {
+            // http://hk.uwenku.com/question/p-vrwwdiql-bnz.html
+            NSWorkspace *workspace = [NSWorkspace sharedWorkspace];
+            NSImage *finderIcon;
+            //= [workspace iconForFile:[workspace absolutePathForAppBundleWithIdentifier:@"com.apple.Finder"]];
+            finderIcon = [workspace iconForFile:[str substringFromIndex:7]];
+            //[finderIcon setSize:NSMakeSize(AppWidth, AppWidth)];
+            [finderIcon setSize:NSMakeSize(self.view.awt.appIconWidthNum.floatValue, self.view.awt.appIconWidthNum.floatValue)];
+            
+            [item.appBT setImage:finderIcon];
+            
+            //self.view.appActiveDic;
+            
+            NSRunningApplication * runningApp = self.view.appActiveDic[str];
+            item.activeIV.hidden = !runningApp;
+            
+            item.runningApp = runningApp;
+        }
         
-        [item.appBT setImage:finderIcon];
-        
-        //self.view.appActiveDic;
-        
-        NSRunningApplication * runningApp = self.view.appActiveDic[str];
-        item.activeIV.hidden = !runningApp;
-        
-        item.runningApp = runningApp;
     }
-    
     item.appPath  = str;
     item.row      = indexPath.item;
     item.delegate = self;
@@ -98,7 +105,23 @@
     //        [AppIconItem.runningApp forceTerminate];
     //    }
     
-    [self.hkt openAppWindows:appIconItem.appPath];
+    if (appIconItem.appPath.length == 0) {
+        [self addAppWithPanel];
+    } else {
+        NSString * str = [appIconItem.appPath stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLFragmentAllowedCharacterSet]];
+        NSURL    * url = [NSURL URLWithString:str];
+        if (url) {
+            if ([NSEvent modifierFlags] & NSEventModifierFlagCommand) {
+                // NSLog(@"按下了 command");
+                // https://stackoom.com/question/39rQu/如果用户在应用程序开始运行之前将其按下-该如何检测是否按下了Shift 判断command键代码
+                NSString * path   = [url.absoluteString substringFromIndex:7].stringByRemovingPercentEncoding;
+                NSString * folder = [path substringToIndex:path.length - url.lastPathComponent.length-1];
+                [[NSWorkspace sharedWorkspace] selectFile:path inFileViewerRootedAtPath:folder];
+            } else {
+                [self.hkt openAppWindows:url.absoluteString];
+            }
+        }
+    }
 }
 
 - (void)delete:(AppIconItem *)appIconItem {
@@ -181,6 +204,43 @@
 }
 
 
+#pragma mark - 新增APP
+- (void)addAppWithPanel {
+    NSOpenPanel *panel = [NSOpenPanel openPanel];
+    
+    [panel setAllowsMultipleSelection:YES];
+    [panel setCanChooseDirectories:NO];
+    [panel setCanChooseFiles:YES];
+    [panel setAllowedFileTypes:@[@"app"]];
+    
+    if ([panel runModal] == NSModalResponseOK) {
+        [self addAppUrlArray:panel.URLs];
+        
+        [self.view updateWindowFrame];
+    }
+}
+
+- (void)addAppUrlArray:(NSArray *)array {
+    for (int i = 0; i<array.count; i++) {
+        NSString * path = [array[i] path];
+        [self addAppPath:path];
+    }
+    
+    [AppInfoTool updateEntity];
+}
+
+- (void)addAppPathArray:(NSArray *)array {
+    for (int i = 0; i<array.count; i++) {
+        NSString * path = array[i];
+        [self addAppPath:path];
+    }
+    
+    [AppInfoTool updateEntity];
+}
+
+- (void)addAppPath:(NSString *)path {
+    [self.view.appInfoEntity.appPathArray addObject:[NSString stringWithFormat:@"file://%@/", path]];
+}
 
 #pragma mark - VC_EventHandler
 
--- FloatDock/FloatDockVC/View/FloatDockVC.m
+++ FloatDock/FloatDockVC/View/FloatDockVC.m
@@ -87,8 +87,10 @@
 }
 
 - (void)updateWindowFrame {
-    CGFloat width  = 20 + self.awt.appIconWidthNum.floatValue *self.appInfoEntity.appPathArray.count + 10*(self.appInfoEntity.appPathArray.count-1);
-    CGFloat height = self.awt.appIconWidthNum.floatValue + 20;
+    NSInteger count = MAX(self.appInfoEntity.appPathArray.count, 1);
+    CGFloat width   = 20 + self.awt.appIconWidthNum.floatValue *count + 10*(count-1);
+    CGFloat height  = self.awt.appIconWidthNum.floatValue + 20;
+    
     [self.view.window setFrame:CGRectMake(self.appInfoEntity.x, self.appInfoEntity.y, width, height) display:YES];
     
     self.infoCvLayout.itemSize = CGSizeMake(self.awt.appIconWidthNum.floatValue, self.awt.appIconWidthNum.floatValue);
@@ -117,7 +119,8 @@
     
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.05 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         
-        CGFloat width = 20 + self.awt.appIconWidthNum.floatValue *self.appInfoEntity.appPathArray.count + 10*(self.appInfoEntity.appPathArray.count-1);
+        NSInteger count = MAX(self.appInfoEntity.appPathArray.count, 1);
+        CGFloat width = 20 + self.awt.appIconWidthNum.floatValue *count + 10*(count -1);
         [self.view.window setFrame:CGRectMake(self.appInfoEntity.x, self.appInfoEntity.y, width, 70) display:YES];
         
         //self.view.frame = CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height);
